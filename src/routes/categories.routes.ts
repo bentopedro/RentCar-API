f1ebc3d25b6e7422b4a7c6c58751a18835f989c3
bentopedro <63908808+bentopedro@users.noreply.github.com>
2022-05-15 01:11:48 +0100

    Inserindo ID com uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
@@ -7,10 +8,15 @@ const categories = [];
 categoriesRoutes.post("/categories", (request, response) => {
     const { name, description } = request.body;
 
-    categories.push({
+    // const id = uuidV4();
+    const category = {
         name,
         description,
-    });
+        id: uuidV4(),
+        created_at: new Date(),
+    };
+
+    categories.push(category);
 
     return response.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,10 @@ app.use(express.json());
 
 app.use(categoriesRoutes);
 
+/*
+PODEMOS PASSAR O NOME DO PRODUCT AQUI MESMO, COMO AQUI E RETIRA-LO NO ROUTES
+// app.use(categoriesRoutes); 
+// app.use("/categories", categoriesRoutes); 
+*/
+
 app.listen(3333, () => console.log("server running"));

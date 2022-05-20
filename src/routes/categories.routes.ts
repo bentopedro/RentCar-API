e84be74e2761b02f61983c3d79600bca09a779bb
bentopedro <63908808+bentopedro@users.noreply.github.com>
2022-05-20 10:41:47 +0100

    Singleton Pattern
    Applied on Repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/categoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/categories", (request, response) => {
     return createCategoryController.handle(request, response);

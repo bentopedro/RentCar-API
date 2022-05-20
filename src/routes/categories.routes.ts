5c981cd4ba395cccb70d24cfdec073fbc13014b5
bentopedro <63908808+bentopedro@users.noreply.github.com>
2022-05-20 09:16:46 +0100

    Create Category UseCase
    - Change services file to UseCase
    - Create a Controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,14 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/categoriesRepository";
-import { PostgreCategoriesRepository } from "../modules/cars/repositories/PostgreCategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 
 const categoriesRepository = new CategoriesRepository();
-// const categoriesRepository = new PostgreCategoriesRepository(); Implementar o repositório para o PostgreSQL
 
 categoriesRoutes.post("/categories", (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(
-        categoriesRepository
-    );
-    createCategoryService.execute({ name, description });
-
-    return response.status(201).send();
+    return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/categories", (request, response) => {

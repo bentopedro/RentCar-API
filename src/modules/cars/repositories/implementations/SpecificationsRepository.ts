9f2bc3fb689b13c60fe36fc82fd3fbc4dbd094a5
bentopedro <63908808+bentopedro@users.noreply.github.com>
2022-05-22 05:58:51 +0100

    Separando Repositório - Interface vs Implementation

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,8 +1,8 @@
-import { Specification } from "../model/Specification";
+import { Specification } from "../../model/Specification";
 import {
     ISpecificationsRepository,
     ISpecificationsRepositoryDTO,
-} from "./ISpecificationsRepository";
+} from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
     private specifications: Specification[];

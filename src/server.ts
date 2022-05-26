93594dc1ede3c08b4cbc582a2969c755fa9ded77
bentopedro <63908808+bentopedro@users.noreply.github.com>
2022-05-26 06:28:34 +0100

    Instalar o swagger-ui-express for API-Docs

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
-
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(categoriesRoutes);
-
 app.use(specificationsRoutes);
 /*
 PODEMOS PASSAR O NOME DO PRODUCT AQUI MESMO, COMO AQUI E RETIRA-LO NO ROUTES

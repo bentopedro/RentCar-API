657cae52beaf7b2ddcf150a372235fbb9bc15c50
Bento Pedro <bentomenni.kidima@gmail.com>
2022-08-24 22:32:03 +0100

    Refactor AuthenticateUser Test

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserInMemory.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserInMemory.spec.ts
@@ -46,16 +46,17 @@ describe("Authenticate User", () => {
 
     it("Should not be able to authenticate with a incorrect password", () => {
         expect(async () => {
-            const user: ICreateUserDTO = {
-                driver_license: "00192",
-                email: "user@test.com",
-                name: "User Test Error",
-                password: "1234",
-            };
+            // const user: ICreateUserDTO = {
+            //     driver_license: "00192",
+            //     email: "user@test.com",
+            //     name: "User Test Error",
+            //     password: "1234",
+            // };
 
-            await createUserUseCase.execute(user);
+            // await createUserUseCase.execute(user);
             await authenticateUserUseCase.execute({
-                email: user.email,
+                // email: user.email,
+                email: "user@test.com",
                 password: "incorrectpassword",
             });
         }).rejects.toBeInstanceOf(AppError);
